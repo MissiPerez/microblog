3e36e489bedd63b880a7ca59bfc345f9a0ca7055
Missi Perez <perezmissi@aol.com>
2019-07-26 15:04:18 -0400

    Implemented mega section 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from app.routes import app
-from flask import render_template
+from app import routes
